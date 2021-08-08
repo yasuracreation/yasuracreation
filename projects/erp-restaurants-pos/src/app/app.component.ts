91f26f9370d4c523c6a8eedb12bfb15298e4161a
Yasura <yasuracreation@gmail.com>
2021-08-08 15:20:33 +0530

    structure add res pos

---
+++ projects/erp-restaurants-pos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'erp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'erp-restaurants-pos';
+}
