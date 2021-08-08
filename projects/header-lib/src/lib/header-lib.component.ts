4b7821c02a752e549264241212723f7d0809404d
Yasura <yasuracreation@gmail.com>
2021-08-08 14:59:32 +0530

    structure commit

---
+++ projects/header-lib/src/lib/header-lib.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'erp-header-lib',
+  template: ` <p>header-lib works!</p> `,
+  styles: []
+})
+export class HeaderLibComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
